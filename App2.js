25abbd6e4f909af856f818de6a1d92048d982706
abhishek1563 <47556999+abhishek1563@users.noreply.github.com>
2023-06-08 00:12:40 +0530

    2nd lecture

--- App2.js
+++
@@ -1,4 +0,0 @@
-const heading2=React.createElement("h1",{},"Helllo World from React from second root!!!");
-root=ReactDOM.createRoot(document.getElementById("root2"));
-root.render(heading2);
-console.log("Hello world")
