864c87cab1e80814c3797a5822ea0ac198ecfc00
abhishek1563 <47556999+abhishek1563@users.noreply.github.com>
2023-06-08 23:33:31 +0530

    3rd lecture

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom/client';
+import ReactDOM, {createRoot}  from 'react-dom/client';
 
 
-const heading=React.createElement("h1",{},"Helllo World from React!!!");
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading=React.createElement("h1",{},"Helllo World from React!!!");
+const jsxheading=<h1 id="heading">Helllo World from React!!!</h1>
+
+const HeadingComponent=()=>( <div>
+    <h1>React functional component</h1>
+    {console.log(jsxheading)}
+    {jsxheading.props.children}
+    </div>)
+
+const root=createRoot(document.getElementById("root"));
+const root2=createRoot(document.getElementById("root2"));
+root.render(<HeadingComponent/>);
+root2.render(jsxheading)
+// console.log(root2.__scope__)
+// ReactDOM.render(<HeadingComponent/>, document.getElementById("root"))
+// ReactDOM.render(jsxheading, document.getElementById("root2"))
